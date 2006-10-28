abf9b56f6aec499a73955bced082d2a9deae9f56
andreicmaxim <andreicmaxim@37e36937-031c-0410-9810-517a34025429>
2006-10-28 09:38:09 +0000

    Renamed Trunk_final to src.

--- branches/acm/src/PluginInterface/Interface.cs
+++ branches/acm/src/PluginInterface/Interface.cs
@@ -8,45 +8,27 @@ namespace PluginInterface
     public interface rssInterface
     {
         void setDocument(XmlDocument doc);
-
         bool canParse();
-
         string parsedHTML();
-
         void showConfiguration();
-
         void addToToolbar(System.Windows.Controls.ToolBar ToolBar);
-
         string description();
-
         string name();
-
         void getDataBase(DataBaseEngine data);
-
         void setOwner(System.Windows.Window window);
-
         void feedChanged(string name, string category);
-
     }
 
 
     public interface DataBaseEngine
     {
         bool categoryExists(String catName);
-
         bool addCategory(string catName);
-
         String[] getCategories();
-
         bool feedExists(String url);
-
         bool addFeed(String catName, String url, String feedName);
-
         bool addFeed(String catName, String url);
-
         void RegisterEventHandler(EventsClass e);
-        
-
     }
 
     public interface EventsClass
