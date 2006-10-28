65a59b99c3df544cfa62c796f0095373462299dd
andreicmaxim <andreicmaxim@37e36937-031c-0410-9810-517a34025429>
2006-10-28 11:30:00 +0000

    Changed the structure according to Pragmatic Programmers' SVN book.

---
+++ src/feedfusionConsole/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace feedfusion.feedfusionConsole
+{
+	class HelloWorld
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
+}
+
